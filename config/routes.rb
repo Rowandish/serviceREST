9418eb28154a71162e74e57020efdae511935828
rowandish <paolo.guglielmini.vr@gmail.com>
2014-07-08 20:28:03 +0200

    aggiunto versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions"}
-  	get 'me', to: 'users#show'
+
+  namespace :v1, defaults: {format: 'json'} do
+	get 'me', to: 'users#show'
+  end
 
   # resources :cats, :defaults => { :format => 'json'}
 end
