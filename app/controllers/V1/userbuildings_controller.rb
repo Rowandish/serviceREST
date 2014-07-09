5b340f3c806fc27aa98917568c5e23a5f3077ec9
rowandish <paolo.guglielmini.vr@gmail.com>
2014-07-09 18:48:15 +0200

    Primo branch

---
+++ app/controllers/V1/userbuildings_controller.rb
@@ -0,0 +1,10 @@
+module V1
+	class UserBuildingsController < ApplicationController
+		#Appunti:
+		#Creazione di un nuovo UserBuilding: @user = User.last
+		#@user.userbuildings.create(level:1, building_id:3)
+		#notare che devo sempre specificare il building_id a cui mi sto riferendo!
+		#Breve: User.last.userbuildings.create(level:1,building_id:3)
+	end
+end
+
